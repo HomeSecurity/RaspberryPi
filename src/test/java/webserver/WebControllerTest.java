8306c32aeaf95d469e326a75609d0d9357c8e9bf
timmue <muenster.tim@googlemail.com>
2017-07-07 17:23:29 +0200

    fixed example tests

--- src/test/java/webserver/WebControllerTest.java
+++ src/test/java/webserver/WebControllerTest.java
@@ -3,20 +3,14 @@ package webserver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
 import webserver.RequestClasses.Credentials;
-import webserver.ResponseClasses.BooleanOutput;
-
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionContext;
-
-import java.util.Enumeration;
-
 import static org.junit.Assert.*;
 
-@SpringBootTest("Mock")
+//EXAMPLE TEST CASES FOR THE WEBCONTROLLER
 public class WebControllerTest {
 
     WebController controller;
@@ -24,29 +18,36 @@ public class WebControllerTest {
     HttpServletResponse response;
     Credentials credentials;
 
+    //create a mocked session and response
+    //initialize the webcontroller and the credentials
     @Before
-    public void setUp(HttpSession session, HttpServletResponse response) throws Exception {
-        this.session = session;
-        this.response = response;
-        controller = new WebController();
-        controller.resetall(session, response);
-        credentials = new Credentials("DefaultUser", "DefaultPassword");
+    public void setUp() throws Exception {
+        this.session = new MockHttpSession();
+        this.response = new MockHttpServletResponse();
+        this.credentials = new Credentials("DefaultUser", "DefaultPassword");
+        this.controller = new WebController();
+
+        //initial login
+        if (!controller.login(session, response, credentials).getOutput()) {
+            System.err.println("Not logged in");
+        }
     }
 
     @After
     public void tearDown() throws Exception {
-
+        //reset all data for following tests
+        controller.resetall(session, response);
     }
 
     @Test
     public void testLogin() throws Exception {
-        assertEquals(controller.login(session, response, credentials), new BooleanOutput(true));
+        //this is already done in the startup method but a assert is nevertheless needed
+        assertTrue(controller.login(session, response, credentials).getOutput());
     }
 
     @Test
     public void testChangeCredentials() throws Exception {
-        controller.login(session, response, credentials);
         Credentials newCredentials = new Credentials("ChangedUser", "ChangedPassword");
-        assertEquals(controller.changecredentials(session, response, newCredentials), true);
+        assertTrue(controller.changecredentials(session, response, newCredentials).getOutput());
     }
 }
