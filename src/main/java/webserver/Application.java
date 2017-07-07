ee3afc3f29e450e131232ad3b87b9a214ca9b5f4
timmue <muenster.tim@googlemail.com>
2017-07-07 16:25:22 +0200

    blank startup

--- src/main/java/webserver/Application.java
+++ src/main/java/webserver/Application.java
@@ -16,47 +16,15 @@ public class Application {
 
     public static void main(String args[]) {
 
-        Alarmsystem system = Alarmsystem.getInstance();
+        //RESET MODEL
+        //Alarmsystem.getInstance().resetModel();
 
-        //TODO:RESET ENTFERNEN!!!
-        system.resetModel();
 
-        system.activateRegistrationMode();
-        system.onRegistrationMessage(1,1000,2.0);
-        system.onRegistrationMessage(2,1001,2.0);
-        system.onRegistrationMessage(3,2000,2.0);
-        system.onRegistrationMessage(4,2001,2.0);
-        system.registerIpCamera(5, "192.168.2.4");
-
-        Component sensor1 = system.getComponentById(1);
-        Component sensor2 = system.getComponentById(2);
-        Component aktor1 = system.getComponentById(3);
-        Component aktor2 = system.getComponentById(4);
-        Component kamera = system.getComponentById(5);
-
-        sensor1.setName("sensor1");
-        sensor2.setName("sensor2");
-        aktor1.setName("aktor1");
-        aktor2.setName("aktor2");
-        kamera.setName("kamera");
-
-        Rule rule1 = system.newRule("rule1", true);
-        Rule rule2 = system.newRule("rule2", true);
-
-        system.addComponentToRule(rule1.getId(), sensor1.getId(), true);
-        system.addComponentToRule(rule1.getId(), aktor1.getId(), true);
-        system.addComponentToRule(rule1.getId(), kamera.getId(), true);
-        system.addComponentToRule(rule2.getId(), sensor2.getId(), true);
-        system.addComponentToRule(rule2.getId(), aktor2.getId(), true);
-
-        Alarmsystem.getInstance().setToken("asdf");
-        Alarmsystem.getInstance().onDataMessage(1,true, 2.0);
-
-        /*try {
+        try {
             Radio radio = Radio.getInstance();
         } catch(Exception e) {
             e.printStackTrace();
-        }*/
+        }
         SpringApplication.run(Application.class, args);
     }
 }
